be2536a711b0ff8b46d8f38e7616801e7298c9f3
boyplrack <boyplrack@gmail.com>
2024-11-28 21:40:03 -0500

    add vpc

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Crear una VPC
+# VPC
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
   tags = {
@@ -10,9 +10,33 @@ resource "aws_vpc" "main" {
   }
 }
 
+# Internet Gateway
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-internet-gateway"
+  }
+}
+
+# Tabla de rutas públicas
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+# Subnet en la zona us-east-1a
 resource "aws_subnet" "public_a" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.3.0/24" 
+  cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "us-east-1a"
   tags = {
@@ -20,9 +44,10 @@ resource "aws_subnet" "public_a" {
   }
 }
 
+# Subnet en la zona us-east-1b
 resource "aws_subnet" "public_b" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.4.0/24"
+  cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "us-east-1b"
   tags = {
@@ -30,7 +55,18 @@ resource "aws_subnet" "public_b" {
   }
 }
 
-# Grupo de seguridad para las instancias y el ALB
+# Asociaciones de la tabla de rutas públicas con las subnets
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public.id
+}
+
+# Grupo de seguridad
 resource "aws_security_group" "web_sg" {
   vpc_id = aws_vpc.main.id
 
